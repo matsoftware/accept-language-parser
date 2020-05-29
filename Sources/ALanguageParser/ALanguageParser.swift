04e32d8276645da4630ca9c25b1c32d496fea57d
Mattia Campolese <info@matsoftware.net>
2020-05-29 15:57:39 +0200

    Initial implementation of ALanguageParser.parse method

--- Sources/ALanguageParser/ALanguageParser.swift
+++ Sources/ALanguageParser/ALanguageParser.swift
@@ -1,3 +1,41 @@
-struct ALanguageParser {
-    var text = "Hello, World!"
+//
+//   ALanguageParser.swift
+//
+//
+//  Created by Mattia Campolese on 29/05/2020.
+//
+
+import Foundation
+
+/// HTTP RFC-2616 Accept-Language parser
+/// https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.4
+/// Language tags are defined by https://www.w3.org/Protocols/rfc2616/rfc2616-sec3.html#sec3.10
+/// where  any two-letter primary-tag is an ISO-639 language abbreviation and any two-letter initial subtag is an ISO-3166 country code.
+public struct ALanguageParser {
+
+    private struct PreferredLanguage {
+        var language: String
+        var weight: Float
+
+        init(_ rawString: String) {
+            let comp = rawString.split(separator: ";")
+            language = String(comp.first ?? "")
+            let rawWeight = String(comp.last ?? "1.0").replacingOccurrences(of: "q=", with: "")
+            weight = Float(rawWeight) ?? 1.0
+        }
+    }
+
+    /// Parses and returns the list of user accepted languagges
+    /// - Parameter acceptLanguage: A valid RFC-2616 Accept-Language string
+    /// - Returns: an ordered list of languages parsed from the `acceptLanguage` string
+    public static func parse(_ acceptLanguage: String) -> [String] {
+        acceptLanguage.split(separator: ",").compactMap {
+            PreferredLanguage(String($0))
+        }.sorted {
+            $0.weight > $1.weight
+        }.map {
+            $0.language
+        }
+    }
+
 }
--- Tests/ALanguageParserTests/ALanguageParserTests.swift
+++ Tests/ALanguageParserTests/ALanguageParserTests.swift
@@ -1,12 +1,24 @@
 import XCTest
-@testable import ALanguageParser
+import ALanguageParser
 
 final class ALanguageParserTests: XCTestCase {
-    func testExample() {
-        XCTAssertEqual(ALanguageParser().text, "Hello, World!")
+
+    //MARK: Valid strings
+    func testParseShouldReturnExpectedOrderedLanguages() {
+
+        let assertions = [
+            (input: "it-IT", expectation: ["it-IT"]),
+            (input: "it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6", expectation: ["it-IT", "it", "en-US","en", "es"]),
+            (input: "en-US,en;q=0.8", expectation: ["en-US", "en"]),
+            (input: "it,nn-NO;q=0.9,zh-Hant-HK;q=0.7", expectation: ["it", "nn-NO", "zh-Hant-HK"]),
+        ]
+
+        assertions.forEach {
+            XCTAssertEqual(ALanguageParser.parse($0.input), $0.expectation)
+        }
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testParseShouldReturnExpectedOrderedLanguages", testParseShouldReturnExpectedOrderedLanguages),
     ]
 }
