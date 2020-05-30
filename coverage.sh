e7b99cc0874dc4f7780809e012367bdfcab6f5b0
Mattia Campolese <info@matsoftware.net>
2020-05-30 18:26:07 +0200

    Initial implementation of CodeCov

--- coverage.sh
+++ coverage.sh
@@ -5,4 +5,4 @@ set -e
 swift test --enable-code-coverage
 
 # Convert format
-llvm-cov export -format="lcov" .build/debug/ALanguageParserPackageTests.xctest/Contents/MacOS/ALanguageParserPackageTests -instr-profile .build/debug/codecov/default.profdata > info.lcov
+llvm-cov export -format="lcov" .build/debug/ALanguageParserPackageTests.xctest -instr-profile .build/debug/codecov/default.profdata > info.lcov
