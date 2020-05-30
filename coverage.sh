a2b8f8a3eb148e347645c314189473887a1b4b0f
Mattia Campolese <matsoftware@users.noreply.github.com>
2020-05-30 21:35:13 +0200

    Finishing Codecov setup (#2)

--- coverage.sh
+++ coverage.sh
@@ -6,3 +6,5 @@ swift test --enable-code-coverage
 
 # Convert format
 llvm-cov export -format="lcov" .build/debug/ALanguageParserPackageTests.xctest -instr-profile .build/debug/codecov/default.profdata > info.lcov
+
+cat info.lcov
