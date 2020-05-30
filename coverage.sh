87c00ea912eb572ccefdcb21069406e82eee2f7c
Mattia Campolese <info@matsoftware.net>
2020-05-30 18:56:29 +0200

    Initial implementation of CodeCov

--- coverage.sh
+++ coverage.sh
@@ -6,10 +6,3 @@ swift test --enable-code-coverage
 
 # Convert format
 llvm-cov export -format="lcov" .build/debug/ALanguageParserPackageTests.xctest -instr-profile .build/debug/codecov/default.profdata > info.lcov
-
-# Code coverage
-# request codecov to detect CI environment to pass through to docker
-ci_env=`bash <(curl -s https://codecov.io/env)`
-docker run $ci_env
-# exec tests
-bash <(curl -s https://codecov.io/bash)
